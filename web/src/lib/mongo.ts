784a64e7324a8751e38509224566d8db96c1f94d
Henrique Pinheiro <henrique@gampoc.com>
2023-04-29 20:41:06 -0300

    Adiciona `listTaxaPaginated`

--- web/src/lib/mongo.ts
+++ web/src/lib/mongo.ts
@@ -24,6 +24,28 @@ export async function listTaxa(
     .toArray()
 }
 
+export async function listTaxaPaginated(
+  filter: Record<string, unknown> = {},
+  page = 0,
+  projection: Record<string, unknown> = {}
+) {
+  const taxa = await getCollection('dwc2json', 'taxa')
+  const total = await taxa.countDocuments(filter)
+  const totalPages = Math.ceil(total / 50)
+  const data = await taxa
+    .find(filter)
+    .project(projection)
+    .sort({ scientificName: 1 })
+    .skip(page * 50)
+    .limit(50)
+    .toArray()
+  return {
+    data,
+    total,
+    totalPages
+  }
+}
+
 export async function countTaxa(filter: Record<string, unknown> = {}) {
   const taxa = await getCollection('dwc2json', 'taxa')
   return await taxa.countDocuments(filter)
