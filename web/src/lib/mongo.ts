ed49af73e091a1e5b6d4fdc1bd27f33de86f3eb5
Henrique Pinheiro <henrique@gampoc.com>
2023-04-29 10:42:26 -0300

    Funcionalidade de busca inicial

--- web/src/lib/mongo.ts
+++ web/src/lib/mongo.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from 'mongodb'
-
-// Connection URL
-const url = process.env.MONGO_URI ?? ''
+const url = import.meta.env.MONGO_URI ?? process.env.MONGO_URI
+if (!url) {
+  throw new Error(
+    'Please define the MONGO_URI environment variable inside .env.local'
+  )
+}
 const client = new MongoClient(url)
 
 async function getCollection(dbName: string, collection: string) {
@@ -9,9 +12,21 @@ async function getCollection(dbName: string, collection: string) {
   return client.db(dbName).collection(collection)
 }
 
-export async function listTaxa(filter: Record<string, unknown> = {}) {
+export async function listTaxa(
+  filter: Record<string, unknown> = {},
+  projection: Record<string, unknown> = {}
+) {
   const taxa = await getCollection('dwc2json', 'taxa')
-  return await taxa.find(filter).toArray()
+  return await taxa
+    .find(filter)
+    .project(projection)
+    .sort({ scientificName: 1 })
+    .toArray()
+}
+
+export async function countTaxa(filter: Record<string, unknown> = {}) {
+  const taxa = await getCollection('dwc2json', 'taxa')
+  return await taxa.countDocuments(filter)
 }
 
 export async function getTaxon(id: string) {
