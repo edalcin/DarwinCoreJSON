7653652b537e7d0728a3abe14613807e5afe4e21
Henrique Pinheiro <henrique@gampoc.com>
2023-10-30 21:30:03 -0300

    tracking(matomo): Adiciona tracking de visitação
    Fix #37 Tracking de visitação

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-
 import deno from '@astrojs/deno'
+import partytown from '@astrojs/partytown'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
   adapter: deno()
 })
