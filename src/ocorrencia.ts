20b8ed490a7b3915f4da375fb2d80c33c15969fc
Henrique Pinheiro <henrique@gampoc.com>
2023-11-05 15:24:06 -0300

    feat(mongo-import): Importa ocorrências
    Usando referências.txt

--- src/ocorrencia.ts
+++ src/ocorrencia.ts
@@ -4,8 +4,8 @@ import { MongoClient } from 'https://deno.land/x/mongo@v0.31.2/mod.ts'
 import { processaZip } from './lib/dwca.ts'
 const { MONGO_URI } = config()
 
-const json = await processaZip(
-  'https://ipt.jbrj.gov.br/jbrj/archive.do?r=jbrj_rb'
+const refUrls = await Deno.readTextFile('./referencias/herbarios.txt').then(
+  (contents) => contents.split('\n')
 )
 
 const client = new MongoClient()
@@ -13,13 +13,19 @@ await client.connect(MONGO_URI as string)
 const collection = client.database('dwc2json').collection('ocorrencias')
 console.debug('Cleaning collection')
 console.log(await collection.deleteMany({}))
-console.debug('Inserting ocurrences')
-const ocorrencias = Object.values(json)
-for (let i = 0, n = ocorrencias.length; i < n; i += 5000) {
-  console.log(`Inserting ${i} to ${Math.min(i + 5000, n)}`)
-  await collection.insertMany(ocorrencias.slice(i, i + 5000), {
-    ordered: false
-  })
+
+for (const url of refUrls) {
+  if (!url) continue
+  console.debug(`Processing ${url}`)
+  const json = await processaZip(url)
+  const ocorrencias = Object.values(json)
+  console.debug(`Inserting entries (${ocorrencias.length})`)
+  for (let i = 0, n = ocorrencias.length; i < n; i += 5000) {
+    console.log(`Inserting ${i} to ${Math.min(i + 5000, n)}`)
+    await collection.insertMany(ocorrencias.slice(i, i + 5000), {
+      ordered: false
+    })
+  }
 }
 console.log('Creating indexes')
 await collection.createIndexes({
