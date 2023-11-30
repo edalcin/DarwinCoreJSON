9985079ed86d9a1ed7b10e2399d56a6c782a0b1b
Henrique Pinheiro <henrique@gampoc.com>
2023-11-29 22:29:16 -0300

    Adiciona suporte a preact

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,9 +3,11 @@ import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
+import preact from '@astrojs/preact'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), partytown()],
+  integrations: [tailwind(), partytown(), preact()],
   adapter: deno()
 })
