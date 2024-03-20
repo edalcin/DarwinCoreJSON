dfa019823de1395a7be473ead6206f3d7836e7d1
Henrique Pinheiro <henrique@gampoc.com>
2024-03-20 00:09:05 -0300

    Conserta deployment com Astro 4

--- web/hackfix_astrobuild.ts
+++ web/hackfix_astrobuild.ts
@@ -4,7 +4,10 @@ const op = Deno.args[0]
 
 if (op === 'build') {
   const mongoFile = await Deno.readTextFile(MONGO_FILE)
-  await Deno.writeTextFile(MONGO_FILE, mongoFile.replace('  isDeno', '  true'))
+  await Deno.writeTextFile(
+    MONGO_FILE,
+    mongoFile.replace('  isDeno ? ', '').replace(" : 'mongodb'", '')
+  )
 }
 if (op === 'dev') {
   await new Deno.Command('git', {
